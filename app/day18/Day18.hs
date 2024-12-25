abb3aff7e65c47d3d798cb89b6ec5d47948de3c1
Christopher Coombes <cjbcoombes@gmail.com>
2024-12-24 23:17:55 -0500

    day 18 part 2

--- app/day18/Day18.hs
+++ app/day18/Day18.hs
@@ -1,6 +1,7 @@
 module Day18 where
 
 import Data.List.Split (splitOn)
+import Data.Maybe (isNothing)
 import Data.Sequence ((|>))
 import qualified Data.Sequence as Q
 import qualified Data.Set as S
@@ -22,19 +23,27 @@ numCorrupted = 1024
 inBounds :: (Int, Int) -> Bool
 inBounds (x, y) = 0 <= x && 0 <= y && x <= edgeX && y <= edgeY
 
-bfs1 :: S.Set (Int, Int) -> Q.Seq (Int, Int, Int) -> S.Set (Int, Int) -> Int
-bfs1 _ Q.Empty _ = -1
+bfs1 :: S.Set (Int, Int) -> Q.Seq (Int, Int, Int) -> S.Set (Int, Int) -> Maybe Int
+bfs1 _ Q.Empty _ = Nothing
 bfs1 seen ((x, y, n) Q.:<| xs) blocked
   | not (inBounds (x, y))
       || (x, y) `S.member` seen
       || (x, y) `S.member` blocked =
       bfs1 seen xs blocked
-  | x == edgeX && y == edgeY = n
+  | x == edgeX && y == edgeY = Just n
   | otherwise =
       bfs1
         ((x, y) `S.insert` seen)
         (xs |> (x - 1, y, n + 1) |> (x + 1, y, n + 1) |> (x, y - 1, n + 1) |> (x, y + 1, n + 1))
         blocked
+
+binSearch :: (Int -> Bool) -> Int -> Int -> Maybe Int
+binSearch f mn mx
+  | mn == mx = if f mn then Just mn else Nothing
+  | f mid = binSearch f mn mid
+  | otherwise = binSearch f (mid + 1) mx
+  where
+    mid = (mn + mx) `quot` 2
 
 part1 :: IO ()
 part1 = do
@@ -42,4 +51,8 @@ part1 = do
   print (bfs1 S.empty (Q.singleton (0, 0, 0)) (S.fromList inp))
 
 part2 :: IO ()
-part2 = getInput >>= print
+part2 = do
+  inp <- getInput
+  let f n = bfs1 S.empty (Q.singleton (0, 0, 0)) . S.fromList . take n $ inp
+      Just res = binSearch (isNothing . f) 0 (length inp)
+  print (inp !! (res - 1))
